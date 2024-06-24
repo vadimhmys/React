f9e8749fc0a7acaf408c955487fdfb5473ccbf72
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-24 22:22:54 +0300

    referencing values with refs: building stopwatch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React, { useRef } from 'react'
+import { useRef, useState } from 'react';
 
-export default function Counter() {
-  let ref = useRef(0);
+export default function Stopwatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
 
-  const handleClick = () => {
-    alert(++ref.current);
+  function handleStart() {
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+  }
+
+  let secondPassed = 0;
+  if (startTime !== null && now !== null) {
+    secondPassed = (now - startTime) / 1000;
   }
 
   return (
-    <button onClick={handleClick}>Click</button>
-  )
+    <>
+      <h1>Time passed: {secondPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </>
+  );
 }
