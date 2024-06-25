bb8c9407cbf1f31c5bd586b49f52cb3c1fe7f402
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-25 22:40:36 +0300

    manipulation DOM with refs: using ref callback

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
 import { useState, useRef } from 'react';
 
-export default function Form() {
-  const inputRef = useRef(null);
+export default function CatFriends() {
+  const itemsRef = useRef(null);
+  const [catList, setCatList] = useState(setupCatList);
 
-  function handleClick() {
-    inputRef.current.focus();
+  function scrollToCat(cat) {
+    const map = getMap();
+    const node = map.get(cat);
+    node.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center',
+    });
+  }
+
+  function getMap() {
+    if (!itemsRef.current) {
+      itemsRef.current = new Map();
+    }
+
+    return itemsRef.current;
   }
 
   return (
     <>
-      <input ref={inputRef}/>
-      <button onClick={handleClick}>Focus the input</button>
+      <nav>
+        <button onClick={() => scrollToCat(catList[0])}>Tom</button>
+        <button onClick={() => scrollToCat(catList[5])}>Maru</button>
+        <button onClick={() => scrollToCat(catList[9])}>Jellylorum</button>
+      </nav>
+      <div>
+        <ul>
+          {catList.map((cat) => (
+            <li
+              key={cat}
+              ref={(node) => {
+                const map = getMap();
+                if (node) {
+                  map.set(cat, node);
+                } else {
+                  map.delete(cat);
+                }
+              }}
+            >
+              <img src={cat}/>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
+}
+
+function setupCatList() {
+  const catList = [];
+  for (let i = 0; i < 10; i++) {
+    catList.push('https://loremflickr.com/320/240/cat?lock=' + i);
+  }
+
+  return catList;
 }
