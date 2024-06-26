b5b7139099038fada56541f186017f350b32fe81
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-27 00:08:15 +0300

    Accessing another component's DOM nodes: finish task 3

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { flushSync } from 'react-dom';
 
 export default function CatFriends() {
   const [index, setIndex] = useState(0);
+  const selectedRef = useRef(null);
+
+  
+
+  function handleClick() {
+    flushSync(() => {
+      if (index < catList.length - 1) {
+        setIndex(index + 1);
+      } else {
+        setIndex(0);
+      }
+    });
+
+    selectedRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center'
+    });
+  }
+
   return (
     <>
       <nav>
-        <button onClick={() => {
-          if (index < catList.length - 1) {
-            setIndex(index + 1);
-          } else {
-            setIndex(0);
-          }
-        }}>
+        <button
+          onClick={handleClick}>
           Next
         </button>
       </nav>
       <div>
-        <ul>
+        <ul style={{ display: 'flex' }}>
           {catList.map((cat, i) => (
-            <li key={cat.id}>
+            <li
+              ref={index === i ? selectedRef : null}
+              style={{ flex: '1 0 200px' }} 
+              key={cat.id}>
               <img
-                className={
-                  index === i ?
-                    'active' :
-                    ''
-                }
+                className={index === i ? 'active' : ''}
                 src={cat.imageUrl}
                 alt={'Cat #' + cat.id}
               />
@@ -40,7 +55,6 @@ const catList = [];
 for (let i = 0; i < 10; i++) {
   catList.push({
     id: i,
-    imageUrl: 'https://placekitten.com/250/200?image=' + i
+    imageUrl: 'https://placekitten.com/250/200?image=' + i,
   });
 }
-
