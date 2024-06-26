ade244af9663a5dd14de1083ba289b5ebd5f8fe4
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-26 23:28:37 +0300

    Accessing another component's DOM nodes: finish task 1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import { useState, useRef } from 'react';
 
 export default function VideoPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef(null);
 
   function handleClick() {
     const nextIsPlaying = !isPlaying;
     setIsPlaying(nextIsPlaying);
+
+    if (!isPlaying) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  }
+
+  function handlePlay() {
+    setIsPlaying(true);
+  }
+
+  function handlePause() {
+    setIsPlaying(false);
   }
 
   return (
@@ -13,7 +28,7 @@ export default function VideoPlayer() {
       <button onClick={handleClick}>
         {isPlaying ? 'Pause' : 'Play'}
       </button>
-      <video width="250">
+      <video width="250" ref={videoRef} onPlay={handlePlay} onPause={handlePause}>
         <source
           src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
           type="video/mp4"
