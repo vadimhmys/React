42cd74dfce9b5221d67f454b45ca36a6a366af0b
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-27 00:12:19 +0300

    Accessing another component's DOM nodes: start task 4

--- src/App.js
+++ src/App.js
@@ -1,60 +1,13 @@
-import { useState, useRef } from 'react';
-import { flushSync } from 'react-dom';
+import SearchButton from './SearchButton.jsx';
+import SearchInput from './SearchInput.jsx';
 
-export default function CatFriends() {
-  const [index, setIndex] = useState(0);
-  const selectedRef = useRef(null);
-
-  
-
-  function handleClick() {
-    flushSync(() => {
-      if (index < catList.length - 1) {
-        setIndex(index + 1);
-      } else {
-        setIndex(0);
-      }
-    });
-
-    selectedRef.current.scrollIntoView({
-      behavior: 'smooth',
-      block: 'nearest',
-      inline: 'center'
-    });
-  }
-
+export default function Page() {
   return (
     <>
       <nav>
-        <button
-          onClick={handleClick}>
-          Next
-        </button>
+        <SearchButton />
       </nav>
-      <div>
-        <ul style={{ display: 'flex' }}>
-          {catList.map((cat, i) => (
-            <li
-              ref={index === i ? selectedRef : null}
-              style={{ flex: '1 0 200px' }} 
-              key={cat.id}>
-              <img
-                className={index === i ? 'active' : ''}
-                src={cat.imageUrl}
-                alt={'Cat #' + cat.id}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
+      <SearchInput />
     </>
   );
-}
-
-const catList = [];
-for (let i = 0; i < 10; i++) {
-  catList.push({
-    id: i,
-    imageUrl: 'https://placekitten.com/250/200?image=' + i,
-  });
 }
---
+++ src/SearchButton.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SearchButton() {
+  return <button>Search</button>;
+}
