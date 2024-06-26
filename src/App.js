911e9df8040c39afbeff4095ea06681bede7270a
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-26 22:52:09 +0300

    Accessing another component's DOM nodes: Flushing state updates synchronously with flushSync - problem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import { useState, useRef, forwardRef, useImperativeHandle } from 'react';
+import { useState, useRef } from 'react';
 
-const MyInput = forwardRef((props, ref) => {
-  const realInputRef = useRef(null);
-  useImperativeHandle(ref, () => ({
-    focus() {
-      realInputRef.current.focus();
-    }
-  }));
-  return <input {...props} ref={realInputRef}/>
-});
+export default function TodoList() {
+  const listRef = useRef(null);
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState(initialTodos);
 
-export default function MyForm() {
-  const inputRef = useRef(null);
-
-  function handleClick() {
-    inputRef.current.focus();
+  function handleAdd() {
+    const newTodo = {id: nextId++, text: text};
+    setText('');
+    setTodos([...todos, newTodo]);
+    listRef.current.lastChild.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest'
+    });
   }
 
   return (
     <>
-      <MyInput ref={inputRef}/>
-      <button onClick={handleClick}>Focus the input</button>
+      <button onClick={handleAdd}>Add</button>
+      <input value={text} onChange={e => setText(e.target.value)}/>
+      <ul ref={listRef}>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </>
   );
+}
+
+let nextId = 0;
+let initialTodos = [];
+
+for (let i = 0; i < 50; i++) {
+  initialTodos.push({
+    id: nextId++,
+    text: 'Todo #' + (i + 1)
+  });
 }
