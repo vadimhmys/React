cb56803fa970afc8b3e9ddb163304d63db1f32ef
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-26 22:15:30 +0300

    Accessing another component's DOM nodes: problem

--- src/App.js
+++ src/App.js
@@ -1,62 +1,20 @@
 import { useState, useRef } from 'react';
 
-export default function CatFriends() {
-  const itemsRef = useRef(null);
-  const [catList, setCatList] = useState(setupCatList);
+function MyInput(props) {
+  return <input {...props} />
+}
 
-  function scrollToCat(cat) {
-    const map = getMap();
-    const node = map.get(cat);
-    node.scrollIntoView({
-      behavior: 'smooth',
-      block: 'nearest',
-      inline: 'center',
-    });
-  }
+export default function MyForm() {
+  const inputRef = useRef(null);
 
-  function getMap() {
-    if (!itemsRef.current) {
-      itemsRef.current = new Map();
-    }
-
-    return itemsRef.current;
+  function handleClick() {
+    inputRef.current.focus();
   }
 
   return (
     <>
-      <nav>
-        <button onClick={() => scrollToCat(catList[0])}>Tom</button>
-        <button onClick={() => scrollToCat(catList[5])}>Maru</button>
-        <button onClick={() => scrollToCat(catList[9])}>Jellylorum</button>
-      </nav>
-      <div>
-        <ul>
-          {catList.map((cat) => (
-            <li
-              key={cat}
-              ref={(node) => {
-                const map = getMap();
-                if (node) {
-                  map.set(cat, node);
-                } else {
-                  map.delete(cat);
-                }
-              }}
-            >
-              <img src={cat}/>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <MyInput ref={inputRef}/>
+      <button onClick={handleClick}>Focus the input</button>
     </>
   );
-}
-
-function setupCatList() {
-  const catList = [];
-  for (let i = 0; i < 10; i++) {
-    catList.push('https://loremflickr.com/320/240/cat?lock=' + i);
-  }
-
-  return catList;
 }
