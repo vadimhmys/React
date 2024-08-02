3dd1ca04c86f430a9e38cfb6e88985873a692717
Vadim Hmyz <vadimhmyz@gmail.com>
2024-08-02 19:08:46 +0300

    delete all

--- src/SearchButton.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function SearchButton({onClick}) {
-  return <button onClick={onClick}>Search</button>;
-}
--- src/SearchInput.jsx
+++
@@ -1,7 +0,0 @@
-import React, { forwardRef } from 'react';
-
-export default forwardRef(
-  function SearchInput(props, ref) {
-    return <input placeholder="Looking for something?" ref={ref}/>;
-  }
-);
